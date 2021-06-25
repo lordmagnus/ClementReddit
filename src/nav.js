cb8e6a4e2e5455d83f1bbbc6db6cdb45f9813de5
arthur degeorges <arthur.degeorges@outremondestudio.eu>
2021-06-25 17:21:27 +0200

    adding navbar

---
+++ src/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Nav extends React.Component {
+    render() {
+        return (
+          <nav class="introNav">
+              <h1>NavBar</h1>
+              <input type="text" id="search" name="search"></input>
+              </nav>
+        );
+      }
+}
